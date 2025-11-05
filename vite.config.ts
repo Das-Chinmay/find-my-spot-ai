48dbee14d6e664fa2f12d563dc0b361b83221aca
CHINMAY <89741289+Das-Chinmay@users.noreply.github.com>
2025-11-05 00:18:01 -0800

    Set base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/find-my-spot-ai/',
       server: {
         port: 3000,
         host: '0.0.0.0',
